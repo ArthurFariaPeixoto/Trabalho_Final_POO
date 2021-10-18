6686d24bec1f96e360bf3a5891d02148eced6c49
Arthur Faria Peixoto <arthurfpeixoto@gmail.com>
2021-10-18 19:35:34 -0300

    .

--- src/Tela.java
+++ src/Tela.java
@@ -1,15 +1,21 @@
 import javax.swing.*;
 
 public class Tela extends JFrame {
-    private final int WIDTH = 1080, HEIGHT = 720;
 
+    private final int WIDTH = 1080, HEIGHT = 720;
     JFrame tela = new JFrame();
 
     public Tela(){
+        criaTela();
+    }
+
+    public void criaTela(){
         tela.setSize(WIDTH, HEIGHT);
-        tela.setTitle("Tela");
+        tela.setTitle("Gerenciamento de Farmácia");
         tela.setVisible(true);
         tela.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        tela.requestFocus();
+        tela.setLocationRelativeTo(null);
     }
 
 }
