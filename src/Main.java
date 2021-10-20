b34bf6b4f0ac156414ddf8e1e28a77d1d95de00b
Arthur Faria Peixoto <arthurfpeixoto@gmail.com>
2021-10-20 15:38:08 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-public class Main {
+import javax.swing.*;
+
+public class Main{
+
+    protected static JFrame tela = new JFrame();
 
     public static void main(String[] args) {
         new Tela();
--- src/Tela.java
+++ src/Tela.java
@@ -1,24 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Tela extends JFrame{
+public class Tela extends JFrame implements ActionListener {
 
     private final int WIDTH = 1080, HEIGHT = 720;
-    JFrame tela = new JFrame();
+    JButton botaoCliente = new JButton("Cliente");
 
     public Tela(){
         criaTela();
     }
 
     public void criaTela(){
-        tela.setSize(WIDTH, HEIGHT);
-        tela.setTitle("Gerenciamento de Farmácia");
-        tela.setVisible(true);
-        tela.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        tela.setFocusable(true);
-        tela.requestFocus();
-        tela.setLocationRelativeTo(null);
-        tela.getContentPane().setBackground(Color.BLACK);
+        setLayout(null);
+
+        Main.tela.setSize(WIDTH, HEIGHT);
+        Main.tela.setTitle("Gerenciamento de Farmácia");
+        Main.tela.setVisible(true);
+        Main.tela.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Main.tela.setFocusable(true);
+        Main.tela.requestFocus();
+        Main.tela.setLocationRelativeTo(null);
+        Main.tela.setResizable(false);
+        Main.tela.getContentPane().setBackground(Color.BLACK);
+
+        botaoCliente.setBounds(10,10,100,50);
+        Main.tela.add(botaoCliente);
+        botaoCliente.addActionListener(this);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == botaoCliente){
+
+        }
 
     }
 }
