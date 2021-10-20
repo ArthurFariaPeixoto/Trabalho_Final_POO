e17728e614bb5f37cc94208980fb85492c655625
Arthur Faria Peixoto <arthurfpeixoto@gmail.com>
2021-10-20 14:52:33 -0300

    .

---
+++ src/Arquivo.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Arquivo {
+
+    public Arquivo() {
+    }
+
+    public void criaArquivo(String tipo){
+        Path diretorio = Paths.get(tipo+".txt");
+
+        try {
+            Files.createFile(diretorio);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void escreveArquivo(String tipo){
+        Path diretorio = Paths.get(tipo+".txt");
+        String escrita = JOptionPane.showInputDialog("Cadastro de "+tipo);
+        byte[] escrita_emBytes = escrita.getBytes();
+
+        try {
+            Files.write(diretorio,escrita_emBytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void leArquivo(String tipo){
+        Path diretorio = Paths.get(tipo+".txt");
+
+        try {
+            byte[] conteudo = Files.readAllBytes(diretorio);
+            String leitura = new String(conteudo);
+            JOptionPane.showMessageDialog(null, leitura);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void deletaArquivo(String tipo){
+        Path diretorio = Paths.get(tipo+".txt");
+
+        try {
+            Files.delete(diretorio);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
