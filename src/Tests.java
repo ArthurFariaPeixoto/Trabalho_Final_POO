d386a6e6741eea01cd8639458baa8cc9c9cc913a
Arthur Faria Peixoto <arthurfpeixoto@gmail.com>
2021-11-02 10:58:59 -0300

    .

---
+++ src/Tests.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Tests {
+    @Test
+    public void salariototalFuncionario() {
+
+        Funcionario fun = new Funcionario(0, "teste", "10/10/1990", 40, 21, 1000);
+        Double expected = 1002.1;
+        Double atual = fun.getSalario();
+        assertEquals(expected, atual);
+    }
+    @Test
+    public void rendimentoFuncionario(){
+        Funcionario fun = new Funcionario(0, "teste", "10/10/1990", 40, 21, 1000);
+        String expected = fun.rendimentoFuncionario();
+        assertEquals("Excelente", expected);
+    }
+    @Test
+    public void erroNoCadastroCliente(){
+        Funcionario fun = new Funcionario(0, "", "", 0, 0, 0);
+        if (fun.nome == null || fun.nome.equals(" ") || fun.getSalario() == 0 || fun.getVendas() == 0 || fun.getHoras_semanais() == 0 || fun.dataNascimento == null || fun.dataNascimento.equals(" ")) {
+            String expected = " ".trim();
+            assertEquals(expected, fun.nome.trim());
+        }
+    }
+    @Test
+    public void descontoDoProduto(){
+        Produto produto = new Produto("teste", "teste", 50, 50, 4, 0);
+        Double expected = produto.getPreco()-produto.getDesconto();
+        Double atual = produto.getPrecoDesconto();
+        assertEquals(expected, atual);
+    }
+
+}
